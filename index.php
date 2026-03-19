f39f608072651f92bf2f4df4d5526c0f29baa1fb
ajvan25 <ajvankumar25@gmail.com>
2026-03-19 12:08:31 +0100

    change

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ $city = "Salzburg";
 $favorite_color = "Blue";
 
 echo "MY "  . "name " . "is " . $firstname  .  $lastname;
+$sentence = "My name is " . $firstname . "" . $lastname . " and my age is " . $age . "and am living in " . $city . "City and my favorite color is" . "" . $favorite_color;
