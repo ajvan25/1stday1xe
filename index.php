29dfd623614f4926b820199d0ae17493262be3d8
ajvan25 <ajvankumar25@gmail.com>
2026-03-19 12:59:12 +0100

    line formatting

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ $age = 14;
 $city = "Salzburg";
 $favorite_color = "Blue";
 
-echo "MY "  . "name " . "is " . $firstname  .  $lastname;
-$sentence = "My name is " . $firstname . "" . $lastname . " and my age is " . $age . "and am living in " . $city . "City and my favorite color is" . "" . $favorite_color;
+echo "MY "  . "name " . "is " . $firstname  .  " " . $lastname . ". <br>";
+
+$sentence = "My name is " . $firstname . "" . $lastname . " and my age is " . $age . "  " . "and am living in " . $city . "  " . "City and my favorite color is" . " " . $favorite_color;
+echo $sentence;
